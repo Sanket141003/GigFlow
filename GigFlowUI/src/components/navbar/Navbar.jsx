cdadbd7630d7e9156e39f1b2308254ba3b632a51
Sanket Shinde <sanketshinde141003@gmail.com>
2026-06-10 18:38:27 +0530

    feat: update Navbar

--- GigFlowUI/src/components/navbar/Navbar.jsx
+++ GigFlowUI/src/components/navbar/Navbar.jsx
@@ -44,12 +44,12 @@ function Navbar() {
       <div className="container">
         <div className="logo">
           <Link className="link" to="/">
-            <span className="text">fiverr</span>
+            <span className="text">GigFlow</span>
           </Link>
           <span className="dot">.</span>
         </div>
         <div className="links">
-          <span>Fiverr Business</span>
+          <span>GigFlow Business</span>
           <span>Explore</span>
           <span>English</span>
           {!currentUser?.isSeller && <span>Become a Seller</span>}
